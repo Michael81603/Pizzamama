b7d6de26a44b7f0d5dc4c5ab986271f1d16185f2
Michael81603 <fenotianamichael81@gmail.com>
2025-10-18 07:28:52 -0700

    Add requirements.txt

--- pizzamama/pizzamama/settings.py
+++ pizzamama/pizzamama/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'pizzamama.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django3',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
+        'NAME': 'michael81$default',
+        'USER': 'michael81',
+        'PASSWORD': 'maykeul2006',
+        'HOST': 'michael81.mysql.pythonanywhere-services.com',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
     ,
     'sqlite': {
